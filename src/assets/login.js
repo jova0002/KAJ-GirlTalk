144338162d8617175f88602a24b2a8611cf6b287
jova0002 <jova0002@stud.kea.dk>
2025-12-18 17:33:44 +0100

    josie-nav-fix

--- src/assets/login.js
+++ src/assets/login.js
@@ -9,7 +9,7 @@ class Login extends HTMLElement {
     this.html = `<style>
       /****** LOGIN MODAL ******/
       #login-modal {
-          background:black;
+          background:#fff5e9;
           position:fixed;
           width:100vw;
           height:100vh;
@@ -18,16 +18,18 @@ class Login extends HTMLElement {
         padding: 30px;
         max-width: 350px;
         width: 100% !important;
-        background-color: #F7F7F7;
+        background-color: #ffe4e5;
         margin: 0 auto;
         border-radius: 2px;
         box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
         overflow: hidden;
-        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI","Roboto","Helvetica Neue", Arial, sans-serif;
+        font-family: Instrument Sans;
+         color: #401609;
       }
       .loginmodal-container h1 {
         text-align: center;
         font-size: 1.8em;
+        color: #401609;
       }
       .loginmodal-container input[type=submit] {
         width: 100%;
@@ -42,14 +44,14 @@ class Login extends HTMLElement {
         margin-bottom: 10px;
         -webkit-appearance: none;
         background: #fff;
-        border: 1px solid #d9d9d9;
-        border-top: 1px solid #c0c0c0;
+        border: 1px solid #401609;
+        border-top: 1px solid #401609;
         padding: 0 8px;
         box-sizing: border-box;
       }
       input[type=password]:hover {
-        border: 1px solid #b9b9b9;
-        border-top: 1px solid #a0a0a0;
+        border: 1px solid #401609;
+        border-top: 1px solid #401609;
         box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);
       }
       .loginmodal {
@@ -63,22 +65,22 @@ class Login extends HTMLElement {
         border: 0px;
         color: #fff;
         text-shadow: 0 1px rgba(0,0,0,0.1); 
-        background-color: #4d90fe;
+        background-color: #401609;
         padding: 17px 0px;
         font-size: 14px;
       }
       .loginmodal-submit:hover {
         border: 0px;
         text-shadow: 0 1px rgba(0,0,0,0.3);
-        background-color: #357ae8;
+        background-color: #1f3db0;
       }
     </style>
     <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="loginmodal-container">
-          <h1>Login</h1><br>
-          <p>This is a school project</p>
-          <p>The password is <code>kea</code></p>
+          <h1>G I R L T A L K</h1><br>
+          <p>Welcome to our Semester 3 Exam. We have redesigned "GirlTalk's" website and hope you like it</p>
+          <p>The password is <code>girltalk</code></p>
           <form>
             <input type="password" name="pass" placeholder="Password">
             <input type="submit" name="login" class="login loginmodal-submit" value="Login">
@@ -90,7 +92,7 @@ class Login extends HTMLElement {
 
     this.shadowRoot.querySelector("form").addEventListener("submit", (e) => {
       e.preventDefault();
-      if (this.shadowRoot.querySelector("input[name=pass]").value === "kea") {
+      if (this.shadowRoot.querySelector("input[name=pass]").value === "girltalk") {
         document.querySelector("#totally-delete-me").remove();
         localStorage.setItem("iform-totally-logged-in", true);
       }
